eb21a47dba3a76ca043d4d325cbe702508c6e293
SMN947 <betum98@gmail.com>
2023-11-19 18:38:20 -0500

    feat: separated navbar and footer to components

--- resources/views/skeleton.blade.php
+++ resources/views/skeleton.blade.php
@@ -10,10 +10,6 @@
 </head>
 
 <body>
-
-
-
-
     <div class="navbar bg-base-100">
         <div class="flex-1">
             <a class="btn btn-ghost text-xl" href="{{ url('/') }}">
@@ -21,40 +17,9 @@
                 {{ config('app.name') }}
             </a>
         </div>
-        <div class="flex-none">
-            <ul class="menu menu-horizontal px-1">
-                @if (Route::has('login'))
-                @auth
-                <li class="nav-item">
-                    <a href="{{ url('/dashboard') }}" class="nav-link {{ Request::path() == 'dashboard' ? 'active' : '' }}">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('/pos') }}" class="nav-link {{ Request::path() == 'pos' ? 'active' : '' }}">Venta</a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('/products') }}" class="nav-link {{ Request::path() == 'products' ? 'active' : '' }}">Productos</a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('/categories') }}" class="nav-link {{ Request::path() == 'categories' ? 'active' : '' }}">Categorias</a>
-                </li>
-                @else
-                <li class="nav-item">
-                    <a href="{{ route('login') }}" class="nav-link {{ Request::path() == 'login' ? 'active' : '' }}">Log in</a>
-                </li>
+        <x-app.navbar></x-app.navbar>
 
-                @if (Route::has('register'))
-                <li class="nav-item">
-                    <a href="{{ route('register') }}" class="nav-link {{ Request::path() == 'register' ? 'active' : '' }}">Register</a>
-                </li>
-                @endif
-                @endauth
-                @endif
-            </ul>
-        </div>
     </div>
-
-
-
 
     @if (isset($errors) && count($errors) > 0)
     <div class="alert alert-danger" role="alert">
@@ -66,11 +31,7 @@
     </div>
     @endif
     @yield('content')
-    <nav class="navbar fixed-bottom bg-light">
-        <div class="container-fluid justify-content-md-center">
-            <span>SMN947 - dev.1.0</span>
-        </div>
-    </nav>
+    <x-app.footer></x-app.footer>
 </body>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
