54d6690041270f963f1fd93e76d09a7e3da61831
SMN947 <betum98@gmail.com>
2023-11-18 21:14:28 -0500

    feat: Removed unused code

--- resources/views/pos.blade.php
+++ resources/views/pos.blade.php
@@ -92,9 +92,8 @@
                         </div>
                     </div>
                 </div>
-
-                <div class="card-footer">
-                    <button id="closeSell" class="btn btn-success ml-0">Cerrar Venta</button>
+                <div class="card-footer flex justify-end">
+                    <button id="closeSell" class="btn btn-success">Cerrar Venta</button>
                 </div>
             </div>
         </div>
@@ -104,81 +103,80 @@
 
 
 @section('scripts')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js"></script>
-    <script>
-        var _token = "{{ csrf_token() }}"
-        var dataCart = []
-        var tableCart = $('#tableCart');
-        var totalCart = $('#totalCart');
-        var paymentByUser = $('#paymentByUser');
-        var clientCambio = $('#clientCambio');
-        var tableCartHtml = '';
+<script>
+    var _token = "{{ csrf_token() }}"
+    var dataCart = []
+    var tableCart = $('#tableCart');
+    var totalCart = $('#totalCart');
+    var paymentByUser = $('#paymentByUser');
+    var clientCambio = $('#clientCambio');
+    var tableCartHtml = '';
 
-        var datosVenta = {
+    var datosVenta = {
+        total: 0,
+        pago: 0,
+        vueltas: -1
+    }
+
+    function resetCart() {
+        paymentByUser.val('0')
+        $("#newProductAmount").val('');
+        dataCart = [];
+        datosVenta = {
             total: 0,
             pago: 0,
             vueltas: -1
         }
+        renderCart();
+        calculatePayment();
+    }
 
-        function resetCart() {
-            paymentByUser.val('0')
-            $("#newProductAmount").val('');
-            dataCart = [];
-            datosVenta = {
-                total: 0,
-                pago: 0,
-                vueltas: -1
+    $(document).ready(function() {
+        $("#addElement").on("click", addToCart);
+        $("#paymentByUser").on("keyup", calculatePayment);
+        $("#closeSell").on("click", saveVenta);
+        closeSell
+    });
+
+    function addToCart() {
+        // TODO: consultar inventario al seleccionar el producto
+        var producto = $("#newProductProduct").val();
+        var cantidad = $("#newProductAmount").val();
+        producto = JSON.parse(producto);
+
+        var existingProductIndex = dataCart.findIndex(item => item.productId === producto.id);
+        if (existingProductIndex !== -1) {
+            var totalAmount = dataCart[existingProductIndex].amount + parseInt(cantidad);
+
+            if (totalAmount > producto.productStock) {
+                alert(`No se puede vender ya que solo quedan ${producto.productStock} disponibles`);
+                return;
             }
-            renderCart();
-            calculatePayment();
-        }
-
-        $(document).ready(function() {
-            $("#addElement").on("click", addToCart);
-            $("#paymentByUser").on("keyup", calculatePayment);
-            $("#closeSell").on("click", saveVenta);
-            closeSell
-        });
-
-        function addToCart() {
-            // TODO: consultar inventario al seleccionar el producto
-            var producto = $("#newProductProduct").val();
-            var cantidad = $("#newProductAmount").val();
-            producto = JSON.parse(producto);
-
-            var existingProductIndex = dataCart.findIndex(item => item.productId === producto.id);
-            if (existingProductIndex !== -1) {
-                var totalAmount = dataCart[existingProductIndex].amount + parseInt(cantidad);
-
-                if (totalAmount > producto.productStock) {
-                    alert(`No se puede vender ya que solo quedan ${producto.productStock} disponibles`);
-                    return;
-                }
-                dataCart[existingProductIndex].amount = totalAmount;
-                dataCart[existingProductIndex].subTotal += producto.productSellPrice * parseInt(cantidad);
-            } else {
-                if (parseInt(cantidad) > producto.productStock) {
-                    alert(`No se puede vender ya que solo quedan ${producto.productStock} disponibles`);
-                    return;
-                }
-                dataCart.push({
-                    productId: producto.id,
-                    producto: producto.productName,
-                    amount: parseInt(cantidad),
-                    productSellPrice: producto.productSellPrice,
-                    productBuyPrice: producto.productBuyPrice,
-                    subTotal: producto.productSellPrice * parseInt(cantidad),
-                });
+            dataCart[existingProductIndex].amount = totalAmount;
+            dataCart[existingProductIndex].subTotal += producto.productSellPrice * parseInt(cantidad);
+        } else {
+            if (parseInt(cantidad) > producto.productStock) {
+                alert(`No se puede vender ya que solo quedan ${producto.productStock} disponibles`);
+                return;
             }
-            renderCart();
+            dataCart.push({
+                productId: producto.id,
+                producto: producto.productName,
+                amount: parseInt(cantidad),
+                productSellPrice: producto.productSellPrice,
+                productBuyPrice: producto.productBuyPrice,
+                subTotal: producto.productSellPrice * parseInt(cantidad),
+            });
         }
+        renderCart();
+    }
 
-        function renderCart() {
-            tableCartHtml = ''
-            datosVenta.total = 0;
-            dataCart.map((product, index) => {
-                datosVenta.total += product.subTotal;
-                tableCartHtml += `<tr>
+    function renderCart() {
+        tableCartHtml = ''
+        datosVenta.total = 0;
+        dataCart.map((product, index) => {
+            datosVenta.total += product.subTotal;
+            tableCartHtml += `<tr>
                     <td>${++index}</td>
                     <td>${product.producto}</td>
                     <td>${product.amount}</td>
@@ -186,80 +184,58 @@
                     <td>${product.subTotal}</td>
                     <td onclick="removeItem(${index})"><i class="fa fa-trash text-danger"></i></td>
                 </tr>`
+        });
+        calculatePayment();
+        tableCart.html(tableCartHtml)
+        totalCart.html(`$${datosVenta.total}`)
+
+    }
+
+    function removeItem(itemId) {
+        dataCart.splice(itemId - 1, 1);
+        renderCart()
+    }
+
+    function calculatePayment() {
+        let pagoPorUser = paymentByUser.val();
+        datosVenta.vueltas = pagoPorUser - datosVenta.total;
+        clientCambio.html(datosVenta.vueltas);
+        if (datosVenta.vueltas < 0) {
+            clientCambio.addClass("text-danger").removeClass("text-success")
+        } else {
+            clientCambio.removeClass("text-danger").addClass("text-success")
+        }
+    }
+
+    function showInvoice(id) {
+        window.open(`/invoice/${id}`, '_blank');
+    }
+
+    function saveVenta() {
+        if (datosVenta.total > 0 && datosVenta.vueltas >= 0) {
+            $.ajax({
+                method: "POST",
+                url: "/pos",
+                data: {
+                    _token: _token,
+                    cartProducts: dataCart,
+                    total: datosVenta.total
+                }
+            }).done(function(msg) {
+                if (msg.error == 0) {
+                    alert(msg.message);
+                } else {
+                    alert(msg.message);
+                }
+                showInvoice(msg.idVenta);
+                setTimeout(() => {
+                    resetCart();
+                }, 2000);
             });
-            calculatePayment();
-            tableCart.html(tableCartHtml)
-            totalCart.html(`$${datosVenta.total}`)
-
+        } else {
+            alert("Venta vacia")
         }
-
-        function removeItem(itemId) {
-            dataCart.splice(itemId - 1, 1);
-            renderCart()
-        }
-
-        function calculatePayment() {
-            let pagoPorUser = paymentByUser.val();
-            datosVenta.vueltas = pagoPorUser - datosVenta.total;
-            clientCambio.html(datosVenta.vueltas);
-            if (datosVenta.vueltas < 0) {
-                clientCambio.addClass("text-danger").removeClass("text-success")
-            } else {
-                clientCambio.removeClass("text-danger").addClass("text-success")
-            }
-        }
-
-        function showInvoice(id) {
-            window.open(`/invoice/${id}`, '_blank');
-            // const invoice = this.document.getElementById("invoice");
-            // var opt = {
-            //     margin: 0.5,
-            //     filename: 'myfile.pdf',
-            //     image: {
-            //         type: 'jpeg',
-            //         quality: 0.98
-            //     },
-            //     html2canvas: {
-            //         scale: 2
-            //     },
-            //     jsPDF: {
-            //         unit: 'mm',
-            //         format: 'a4',
-            //         orientation: 'portrait'
-            //     }
-            // };
-
-            // html2pdf().from(invoice).set(opt).toPdf().get('pdf').then(function(pdfObj) {
-            //     pdfObj.autoPrint();
-            //     window.open(pdfObj.output('bloburl'), '_blank');
-            // });
-        }
-
-        function saveVenta() {
-            if (datosVenta.total > 0 && datosVenta.vueltas >= 0) {
-                $.ajax({
-                    method: "POST",
-                    url: "/pos",
-                    data: {
-                        _token: _token,
-                        cartProducts: dataCart,
-                        total: datosVenta.total
-                    }
-                }).done(function(msg) {
-                    if (msg.error == 0) {
-                        alert(msg.message);
-                    } else {
-                        alert(msg.message);
-                    }
-                    showInvoice(msg.idVenta);
-                    setTimeout(() => {
-                        resetCart();
-                    }, 2000);
-                });
-            } else {
-                alert("Venta vacia")
-            }
-        }
-        renderCart();
-    </script>
-    @endsection
+    }
+    renderCart();
+</script>
+@endsection
