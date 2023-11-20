8737d29c7bf5bbaec326f88059f36999f77d27f5
SMN947 <betum98@gmail.com>
2023-11-19 22:25:44 -0500

    feat: Migrated Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,179 +1,145 @@
 @extends('skeleton')
 @section('scripts')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
 <script>
     $(document).ready(function() {
         $('#inventory').DataTable();
-        $('#salesResumee').DataTable();
+        $('#salesResume').DataTable();
         $('#topSell').DataTable();
     });
 </script>
 @endsection
 @section('content')
-<div class="container-fluid">
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generar Reporte</a>
+<div class="container m-auto">
+    <div class="grid grid-cols-1 sm:grid-cols-5 gap-4 my-4">
+        <div></div>
+        <form action="{{ route('dashboard.index') }}" method="GET" class="col-span-3">
+            <div class="join grid grid-cols-5 gap-4">
+                <div class="form-control w-full max-w-xs col-span-2">
+                    <label class="label">
+                        <span class="label-text">Fecha de Inicio</span>
+                    </label>
+                    <input type="date" id="start_date" name="start_date" class="input input-bordered w-full max-w-xs" value="{{ $startDate->format('Y-m-d') }}" />
+                </div>
+                <div class="form-control w-full max-w-xs  col-span-2">
+                    <label class="label">
+                        <span class="label-text">Fecha de Fin</span>
+                    </label>
+                    <input type="date" id="end_date" name="end_date" class="input input-bordered w-full max-w-xs" value="{{ $endDate->format('Y-m-d') }}" />
+                </div>
+                <div class="mt-auto mx-2 w-full max-w-xs  col-span-1">
+                    <button class="btn btn-outline w-full">Filtrar</button>
+                </div>
+            </div>
+        </form>
     </div>
-
-    <form action="{{ route('dashboard.index') }}" method="GET" class="mb-3">
-        <div class="row">
-            <div class="col">
-                <label for="start_date">Fecha de inicio:</label>
-                <input type="date" id="start_date" name="start_date" class="form-control" value="{{ $startDate->format('Y-m-d') }}">
-            </div>
-            <div class="col">
-                <label for="end_date">Fecha de fin:</label>
-                <input type="date" id="end_date" name="end_date" class="form-control" value="{{ $endDate->format('Y-m-d') }}">
-            </div>
-            <div class="col">
-                <br />
-                <button type="submit" class="btn btn-primary">Filtrar</button>
-            </div>
-        </div>
-    </form>
-
-
-    <!-- Content Row -->
-    <div class="row">
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-6 col-md-6 mb-4">
-            <div class="card border-start border-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Ganancias (Mensual)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-6 col-md-6 mb-4">
-            <div class="card border-start border-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Ganancia (Semanal)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 my-4">
+        <x-layout.card>
+            <x-layout.card.body>
+                <h1 class="text-lg font-bold">Ganancias Mes</h1>
+                <p class="">$xx.xxx.xxx</p>
+            </x-layout.card.body>
+        </x-layout.card>
+        <x-layout.card>
+            <x-layout.card.body>
+                <h1 class="text-lg font-bold">Ganancias Semana</h1>
+                <p class="">$xx.xxx.xxx</p>
+            </x-layout.card.body>
+        </x-layout.card>
+        <x-layout.card>
+            <x-layout.card.body>
+                <h1 class="text-lg font-bold">Ventas</h1>
+                <p class="">xxx</p>
+            </x-layout.card.body>
+        </x-layout.card>
     </div>
-
-    <!-- Content Row -->
-
-    <div class="row">
-
-        <!-- RESUMEN DE VENTAS -->
-        <div class="col-xl-6 col-md-12 mb-4">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Resumen de ventas</h6>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <table class="table table-bordered" id="salesResumee">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Total</th>
-                                <th>Fecha</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($sales as $sale)
-                            <tr>
-                                <td>{{ $sale->id }}</td>
-                                <td>{{ $sale->total }}</td>
-                                <td>{{ $sale->created_at }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-
-                </div>
-            </div>
-        </div>
-
-        <!-- MAS VENDIDOS -->
-        <div class="col-xl-6 col-md-12 mb-4">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Mas vendidos</h6>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <table class="table table-bordered" id="topSell">
-                        <thead>
-                            <tr>
-                                <th>Producto</th>
-                                <th>Cantidad</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($topSell as $product)
-                            <tr>
-                                <td>{{ $product->productName }}</td>
-                                <td>{{ $product->amount }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-
-        <!-- MAS VENDIDOS -->
-        <div class="col-xl-6 col-md-12 mb-4">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Estado de inventario</h6>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <table class="table table-bordered" id="inventory">
-                        <thead>
-                            <tr>
-                                <th>Producto</th>
-                                <th>Stock Actual</th>
-                                <th>Minimo</th>
-                                <th>Delta</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($stock as $product)
-                            <tr class="bg-{{ $product->toMinStock <= 0 ? 'danger' : 'success' }}">
-                                <td>{{ $product->productName }}</td>
-                                <td>{{ $product->productStock }}</td>
-                                <td>{{ $product->productMinStock }}</td>
-                                <td>{{ $product->toMinStock }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+    <!-- INICIO RESUMEN DE VENTAS -->
+    <div class="grid grid-cols-1 gap-4 my-4">
+        <x-layout.card>
+            <x-layout.card.header>
+                Resumen de ventas
+            </x-layout.card.header>
+            <x-layout.card.body>
+                <table class="table bg-base-100" id="salesResume">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Total</th>
+                            <th>Fecha</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($sales as $sale)
+                        <tr class="hover:bg-base-300">
+                            <td>{{ $sale->id }}</td>
+                            <td>{{ $sale->total }}</td>
+                            <td>{{ $sale->created_at }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </x-layout.card.body>
+        </x-layout.card>
     </div>
+    <!-- FIN RESUMEN DE VENTAS -->
+    <!-- INICIO MAS VENDIDOS -->
+    <div class="grid grid-cols-1 gap-4 my-4">
+        <x-layout.card>
+            <x-layout.card.header>
+                Mas Vendidos
+            </x-layout.card.header>
+            <x-layout.card.body>
+                <table class="table bg-base-100" id="topSell">
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Cantidad</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($topSell as $product)
+                        <tr>
+                            <td>{{ $product->productName }}</td>
+                            <td>{{ $product->amount }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </x-layout.card.body>
+        </x-layout.card>
+    </div>
+    <!-- FIN MAS VENDIDOS -->
+    <!-- INICIO INVENTARIO -->
+    <div class="grid grid-cols-1 gap-4 my-4">
+        <x-layout.card>
+            <x-layout.card.header>
+                Estado de inventario
+            </x-layout.card.header>
+            <x-layout.card.body>
+                <table class="table bg-base-100" id="inventory">
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Stock Actual</th>
+                            <th>Minimo</th>
+                            <th>Delta</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($stock as $product)
+                        <tr class="bg-{{ $product->toMinStock <= 0 ? 'danger' : 'success' }}">
+                            <td>{{ $product->productName }}</td>
+                            <td>{{ $product->productStock }}</td>
+                            <td>{{ $product->productMinStock }}</td>
+                            <td>{{ $product->toMinStock }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </x-layout.card.body>
+        </x-layout.card>
+    </div>
+    <!-- FIN INVENTARIO -->
 </div>
 @endsection
