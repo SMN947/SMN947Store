48744c420359ff3b1c4d9882fdbdc1fbb0cd98a5
SMN947 <betum98@gmail.com>
2023-11-25 21:32:40 -0500

    feat: Migrated categoies

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,112 +1,86 @@
 @extends('skeleton')
 
 @section('content')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-    <div class="container-fluid">
+<form action="/categories" method="POST">
+    <div class="container m-auto my-4">
+        <x-layout.card>
+            <x-layout.card.header>
+                Creacion de Categoria
+            </x-layout.card.header>
+            <x-layout.card.body>
+                @csrf
+                <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 my-4">
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Nombre de la categoria</span>
+                        </label>
+                        <input type="text" name="categoryName" class="input input-bordered w-full max-w-xs" />
 
-
-        <!-- Content Row -->
-        <div class="row">
-
-            <!-- Content Column -->
-            <div class="col-12">
-
-                <!-- Project Card Example -->
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-default">Crear Categoria</h6>
-                    </div>
-                    <div class="card-body">
-                        <form action="/categories" method="POST">
-                            @csrf
-                            <div class="row">
-                                <div class="col">
-                                    <div class="mb-3">
-                                        <label class="form-label">Nombre de la categoria</label>
-                                        <input type="text" name="categoryName" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                            <input class="btn btn-success float-right" type="submit" value="Crear">
-                        </form>
                     </div>
                 </div>
-
-            </div>
-        </div>
-
-        <!-- Content Row -->
-        <div class="row">
-
-            <!-- Content Column -->
-            <div class="col-12">
-
-                <!-- Project Card Example -->
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-default">Listado de Categorias</h6>
-                    </div>
-                    <div class="card-body">
-                        <table class="table table-bordered">
-                            <thead>
-
-                            </thead>
-                            <tbody>
-                                @foreach ($categories as $category)
-                                    <tr>
-                                        <td>{{ $category->id }}</td>
-                                        <td>{{ $category->categoryName }}</td>
-                                        <td>
-                                            <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                                data-bs-target="#modal{{ $category->id }}">Eliminar</button>
-                                        </td>
-                                    </tr>
-
-                                    <!-- MODAL -->
-                                    <div class="modal fade" id="modal{{ $category->id }}" tabindex="-1"
-                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Eliminar categoría</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <p>
-                                                        Al eliminar la categoría
-                                                        <strong>{{ $category->categoryName }}</strong>
-                                                        se eliminan todas las tareas asignadas a la misma.
-                                                    </p>
-                                                    ¿Está seguro que desea eliminar la categoría
-                                                    <strong>{{ $category->categoryName }}</strong>?
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary"
-                                                        data-bs-dismiss="modal">No, cancelar</button>
-                                                    <form
-                                                        action="{{ route('categories.destroy', ['category' => $category->id]) }}"
-                                                        method="POST">
-                                                        @method('DELETE')
-                                                        @csrf
-                                                        <button type="submit" class="btn btn-primary">Sí, eliminar
-                                                            categoría</button>
-                                                    </form>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
+            </x-layout.card.body>
+            <x-layout.card.footer>
+                <button class="btn btn-success">Crear Categoria</button>
+            </x-layout.card.footer>
+        </x-layout.card>
     </div>
+</form>
+
+<div class="grid grid-cols-1 gap-4 my-4">
+    <x-layout.card>
+        <x-layout.card.header>
+            Resumen de ventas
+        </x-layout.card.header>
+        <x-layout.card.body>
+            <table class="table bg-base-100" id="salesResume">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Categoria</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category)
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->categoryName }}</td>
+                        <td>
+                            <a class="btn btn-error btn-sm" onclick="modalDelete{{$category->id }}.showModal()">Eliminar</a>
+                        </td>
+                    </tr>
+
+                    <!-- START DELETE MODAL -->
+                    <dialog id="modalDelete{{ $category->id }}" class="modal modal-bottom sm:modal-middle">
+                        <div class="modal-box">
+                            <h3 class="font-bold text-lg">Eliminar Categoria</h3>
+                            <p class="py-4">
+                                ¿Está seguro que desea eliminar la categoria
+                                <strong>{{ $category->categoryName }}</strong>?
+                            </p>
+                            <div class="modal-action">
+                                <button type="button" class="btn btn-secondary" onclick="modalDelete{{$category->id }}.close()">
+                                    No, cancelar
+                                </button>
+                                <form action="{{ route('categories.destroy', ['category' => $category->id]) }}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary">
+                                        Sí, eliminar categoria
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </dialog>
+                    <!-- END DELETE MODAL -->
+                    @endforeach
+                </tbody>
+            </table>
+        </x-layout.card.body>
+        <x-layout.card.footer>
+            <button class="btn btn-success">Crear Categoria</button>
+        </x-layout.card.footer>
+    </x-layout.card>
+
+</div>
 @endsection
