45c8c1551f59c4dc3ddd2e2c13281c3cd23d983f
SMN947 <betum98@gmail.com>
2023-11-26 23:53:13 -0500

    feat: Added Logout

--- resources/views/components/app/navbar.blade.php
+++ resources/views/components/app/navbar.blade.php
@@ -1,11 +1,13 @@
 <div class="navbar bg-base-200">
     <div class="flex-1">
-        <a class="btn btn-ghost text-xl" href="{{ url('/') }}">
+        <a class="btn btn-ghost text-xl" href="{{ url('/'.tenant('path').'/') }}">
             <img src="{{ asset('/images/logo.svg') }}" alt="" width="30" height="30" class="d-inline-block align-text-top">
-            {{ config('app.name') }} - {{tenant('path')}}
+            {{ config('app.name') }}
+            @auth
+            - {{tenant('path')}}
+            @endauth
         </a>
     </div>
-
     <div class="flex-none">
         <ul class="menu menu-horizontal px-1">
             @if (Route::has('login'))
@@ -21,6 +23,14 @@
             </li>
             <li class="nav-item">
                 <a href="{{ url(tenant('path').'/categories') }}" class="nav-link {{ Request::path() == 'categories' ? 'active' : '' }}">Categorias</a>
+            </li>
+            <li class="nav-item">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a href="" class="nav-link" onclick="event.preventDefault(); this.closest('form').submit();">
+                        Cerrar Sesion
+                    </a>
+                </form>
             </li>
             <li>
                 <div class="form-control p-0 m-0">
